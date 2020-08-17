57b8745a4c0a0c496214db30eb0b9bee5026566b
MarianaOlimpia <mariana.olimpiap@gmail.com>
2020-08-17 00:54:39 -0500

    Updates

---
+++ precache-manifest.0f0f330cb4453573205939b97f0d18ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee2851aa56dd71a95bdea37250b01c4",
+    "url": "/data-lovers-react/index.html"
+  },
+  {
+    "revision": "5c5dab0d8fe82a473f43",
+    "url": "/data-lovers-react/static/css/main.03b4a92a.chunk.css"
+  },
+  {
+    "revision": "bd7d0fc2139e18fc95a4",
+    "url": "/data-lovers-react/static/js/2.2655e9eb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/data-lovers-react/static/js/2.2655e9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5dab0d8fe82a473f43",
+    "url": "/data-lovers-react/static/js/main.2fe8a531.chunk.js"
+  },
+  {
+    "revision": "ea20e6ae462218f68a67",
+    "url": "/data-lovers-react/static/js/runtime-main.252c58b1.js"
+  },
+  {
+    "revision": "f0070bc783c9390236cf6f0688aba352",
+    "url": "/data-lovers-react/static/media/imglogo.f0070bc7.jpg"
+  }
+]);
